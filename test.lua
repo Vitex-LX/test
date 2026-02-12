c16457852517471ce9136137d72140824a5cfc99
yasinnaderpour44-stack <yasinnaderpour44@gmail.com>
2026-02-12 14:32:28 +0330

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,21 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+WindUI:AddTheme({
+    Name = "My Theme", -- theme name
+    
+    Accent = Color3.fromHex("#18181b"),
+    Background = Color3.fromHex("#101010"), -- Accent
+    Outline = Color3.fromHex("#FFFFFF"),
+    Text = Color3.fromHex("#FFFFFF"),
+    Placeholder = Color3.fromHex("#7a7a7a"),
+    Button = Color3.fromHex("#52525b"),
+    Icon = Color3.fromHex("#a1a1aa"),
+})
+WindUI:SetTheme("My Theme")
+
+
+local Tab = Window:Tab({
+    Title = "کاربردی",
+    Icon = "bird", -- optional
+    Locked = false,
+})
